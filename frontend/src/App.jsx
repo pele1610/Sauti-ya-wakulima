0edd38dbf38b48f81b8de984bb349cbaab1e1b1a
pele1610 <pelecolo56@gmail.com>
2026-07-29 20:00:35 +0300

    Wire AuthProvider and React Router into App with login/register/protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,30 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import ProtectedRoute from './routes/ProtectedRoute'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
-  return <Home />
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/protected-test"
+            element={
+              <ProtectedRoute>
+                <div className="p-6">You're authenticated 🎉</div>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  )
 }
 
 export default App
